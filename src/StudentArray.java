39c21a961b16a798cb70af1f80e1eb236a1fa058
Supun Subodha <supunmalsingha@gmail.com>
2025-10-05 19:35:20 +0530

    Implemented methods for inserting students and resizing array List when necessory.

--- src/StudentArray.java
+++ src/StudentArray.java
@@ -1,2 +1,24 @@
 public class StudentArray {
+    Student[] student;
+    final int DEFAULTCAPACITY = 10;
+    int studentCount;
+    public StudentArray(){
+        student = new Student[DEFAULTCAPACITY];
+    }
+    public void addStudent(int ID,String name,String degree,int year,float GPA){
+        if(studentCount == student.length){
+            resize();
+        }
+        student[studentCount] = new Student(ID,name,degree,year,GPA);
+        studentCount++;
+
+    }
+    public void resize(){
+        Student[] newStudent = new Student[student.length*2];
+        for(int i=0;i<student.length;i++){
+            newStudent[i] = student[i];
+        }
+        student = newStudent;
+    }
+
 }
