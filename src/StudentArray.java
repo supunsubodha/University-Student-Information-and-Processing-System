f597137aa8e519ece95258ca91845ca382826617
Supun Subodha <supunmalsingha@gmail.com>
2025-10-05 20:34:04 +0530

    Update method

--- src/StudentArray.java
+++ src/StudentArray.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StudentArray {
     Student[] student;
     final int DEFAULTCAPACITY = 10;
@@ -19,6 +21,43 @@ public class StudentArray {
             newStudent[i] = student[i];
         }
         student = newStudent;
+    }
+    public void update(int ID){
+        for(int i=0; i<studentCount;i++){
+            if(student[i].ID==ID){
+                System.out.println("What information about student you need to update?");
+                System.out.println("1.Name\n2.Degree\n3.Year\n4.GPA\nEnter the relevant number here: ");
+                Scanner sc = new Scanner(System.in);
+                int toUpdate = sc.nextInt();
+                switch(toUpdate){
+                    case 1:
+                        System.out.println("Enter new name: ");
+                        student[i].name=sc.next();
+                        break;
+                    case 2:
+                        System.out.println("Enter the new degree: ");
+                        student[i].degree = sc.next();
+                        break;
+                    case 3:
+                        System.out.println("Enter the new year: ");
+                        student[i].year = sc.nextInt();
+                        break;
+                    case 4:
+                        System.out.println("Enter the new GPA: ");
+                        student[i].GPA = sc.nextFloat();
+                        break;
+                    default:
+                        System.out.println("Wrong input, Try again!");
+                        update(ID);
+                        break;
+                }
+                break;
+
+            }
+
+        }
+        System.out.println("Wrong ID, Try again!");
+        update(ID);
     }
 
 }
