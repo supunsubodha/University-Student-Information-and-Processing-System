ede47c0ab1655757260b61b626b7a4ed3ca44361
Supun Subodha <supunmalsingha@gmail.com>
2025-10-05 19:11:01 +0530

    Student Class for storing student details

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    int ID;
+    String name;
+    String degree;
+    int year;
+    float GPA;
+    public Student(int ID, String name,String degree,int year,float GPA){
+        this.ID = ID;
+        this.name = name;
+        this.degree = degree;
+        this.year = year;
+        this.GPA = GPA;
+    }
+}
